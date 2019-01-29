98400dc1d2c323bc496dd7adf1a5aff891926788
CAVE-DEV1\ed94 <egonz467@fiu.edu>
2019-01-28 19:05:14 -0500

    Initial Project Commit.
    Made an unreal project and started to set it up. Need to configure the gitignore.

---
+++ SparkDev_VR2019/Intermediate/Build/Win64/SparkDev_VR2019Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/ed94/Documents/Unreal Projects/SparkDev_VR2019/Intermediate/Build/Win64/SparkDev_VR2019Editor/Development/Engine/SharedPCH.Engine.h"
---
+++ SparkDev_VR2019/Source/SparkDev_VR2019/SparkDev_VR2019.cpp
@@ -0,0 +1,6 @@
+
+
+#include "SparkDev_VR2019.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SparkDev_VR2019, "SparkDev_VR2019" );
